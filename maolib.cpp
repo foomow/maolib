d342815f0f100bae60f7d9073442ec1daaa868dc
Fumao Li <foomow@163.com>
2022-09-27 00:25:48 +0800

    update

--- maolib.cpp
+++ maolib.cpp
@@ -4,10 +4,5 @@ using namespace maolib;
 int main()
 {
 	cout << "nihao chengdu" << endl;
-	Json a;
-	a.test();
-	OpenApiClient b;
-	b.test();
-
 	cout << time::get_now_string() << endl;
 }
--- maolib_json/maolib_json.h
+++ maolib_json/maolib_json.h
@@ -1,11 +1,46 @@
 ﻿#pragma once
 #include <iostream>
+#include <map>
+#include <vector>
+#include <string>
 using namespace std;
 namespace maolib {
-	class Json {
-	public:
-		Json();
-		~Json();
-		void test();
-	};
+	namespace Json {
+		enum J_TYPE {
+			J_INVALID,
+			J_NULL,
+			J_NUMBER,
+			J_STRING,
+			J_ARRAY,
+			J_OBJECT
+		};
+
+		class JsonValue {
+		public:
+			inline J_TYPE GetType() { return _type; }
+			inline std::string ToString() { return _json_str; }
+		private:
+			virtual void _parse(std::string json_str);
+			J_TYPE _type;
+			std::string _json_str;
+		};
+
+		class JObject :public JsonValue {
+		public:
+			JObject();
+			~JObject();
+		private:
+			void _parse(std::string json_str);
+			std::map<std::string, JsonValue> _members;
+		};
+
+		class JArray :public JsonValue {
+		public:
+			JArray();
+			~JArray();
+		private:
+			void _parse(std::string json_str);
+			std::vector<JsonValue> _members;
+		};
+	}
 }
