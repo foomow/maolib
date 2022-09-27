0e53a10015486f8aefab571b6ced084dbfa5efd8
Fumao Li <foomow@163.com>
2022-09-27 23:28:52 +0800

    update

--- maolib.cpp
+++ maolib.cpp
@@ -1,8 +1,13 @@
 #include "maolib.h"
 using namespace std;
 using namespace maolib;
+using namespace maolib::json;
+using namespace maolib::time;
 int main()
 {
 	cout << "nihao chengdu" << endl;
-	cout << time::get_now_string() << endl;
+	cout << get_now_string() << endl;
+	JsonObject a;
+	a.parse("{\"a\":  {\"b\":\"nihao\"}}");
+	cout << a.getJsonString() << endl;
 }
